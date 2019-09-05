794011933fad6f390eda1f1f4ecc728dbbea3b35
Felix K <herubrin@gmail.com>
2019-09-05 12:21:28 +0200

    The probe update, part 1
    - Refactored at least 4 lines of code
    - Added extended probes
    - Introduced actual functions
    - Watched 3 episodes of Star Trek TNG

--- Python/roll.py
+++ Python/roll.py
@@ -8,7 +8,7 @@ import time
 
 Copyright (C) 2019 Felix K.
 
-Version 1.0
+Version 1.1
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
@@ -35,18 +35,135 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
 
+def has_enough_hits(rolls, threshold):
+    # test whether the 5's and 6's are over the needed hit threshold
+    if len(rolls) > 0:
+        return rolls[4] + rolls[5] >= threshold
+    else:
+        return False
+
+
+def dice_roll_normal(sides, rolls, roll_array=None):
+    # check if we have to create a new array or if we are extending a given one
+    if roll_array is None or len(roll_array) == 0:
+        dice_rolls = {}
+
+        # resize the dice roll list to the given dice size
+        for i in range(sides):
+            dice_rolls[i] = 0
+    else:
+        # we already have an array, use the given one
+        dice_rolls = roll_array
+
+    # roll the dice
+    for i in range(rolls):
+        current_roll = random.randint(1, sides)
+        dice_rolls[current_roll - 1] += 1
+
+    # return all results
+    return dice_rolls
+
+
+def dice_roll_probe(roll_amount, threshold, correction):
+    rolls = {}
+    successes = {}
+    twenties = {}
+
+    if roll_amount == 1:
+        roll_amount = 3
+
+    # roll the dice
+    for i in range(roll_amount):
+        rolls[i] = random.randint(1, 20)
+        if rolls[i] == 20:
+            twenties[i] = True
+        else:
+            twenties[i] = False
+
+        if rolls[i] <= threshold:
+            successes[i] = True
+        else:
+            successes[i] = False
+
+    print('ROLLS: {0}'.format(rolls))
+    print('CORRECTION: {0}'.format(correction))
+
+    # this is where we try to turn the highest possible amount of failures into successes
+    # loop while optimization is still possible
+    optimized = False
+    if any(a for a in rolls):
+        print('')
+        print('OPTIMIZING...')
+        while not optimized:
+            lowest_failure = 21
+            failure_id = -1
+            # find the lowest failure
+            print('')
+            print('Determining lowest failed roll...')
+            for i in range(roll_amount):
+                if lowest_failure > rolls[i] > threshold:
+                    lowest_failure = rolls[i]
+                    failure_id = i
+
+            if lowest_failure is not 21:
+                print('New lowest failed roll: {0} (Roll ID {1})'.format(lowest_failure, failure_id))
+
+            if failure_id is not -1:
+                # try to turn it into a success
+                needed_correction = lowest_failure - threshold
+                print('Correction points needed: {0}, points available: {1}'.format(needed_correction, correction))
+                if needed_correction <= correction:
+                    correction = correction - needed_correction
+                    rolls[failure_id] = rolls[failure_id] - needed_correction
+                    successes[failure_id] = True
+                    print('Roll {0} optimized'.format(failure_id))
+                else:
+                    optimized = True
+            else:
+                optimized = True
+
+    print('')
+    print('All rolls optimized')
+
+    # determine final data
+    succs = 0
+    fails = 0
+    fixed_20s = 0
+
+    for i in rolls:
+        if successes[i]:
+            succs = succs + 1
+
+        if rolls[i] == 20:
+            fails = fails + 1
+        elif twenties[i]:
+            fixed_20s = fixed_20s + 1
+
+    # print the final probe result
+    print('')
+    print('FINAL RESULT: {0} SUCCESS(ES), {1} x 20, {2} x CORRECTED 20'.format(succs, fails, fixed_20s))
+
+
 def main():
     # get a parser and set up commands
-    parser = argparse.ArgumentParser(description='This script simulates dice rolls for the Goren DnD System.')
+    parser = argparse.ArgumentParser(description='This script simulates dice rolls for the Goren DnD System.',
+                                     epilog='Starting the script with the --threshold parameter will automatically '
+                                            'switch to probe mode.')
 
     parser.add_argument('--rolls', '-r', help='The amount of rolls (Default: 1)', default=1)
     parser.add_argument('--sides', '-s', help='The amount of sides the dice have (Default: 6)', default=6)
-    parser.add_argument('--showHits', '-sH', help='Whether to show successful rolls (Default: false)',
-                        action='store_true')
+
+    parser.add_argument('--hits', '-H', help='The amount of hits to pass the probe (Default: 0)', default=0)
+    parser.add_argument('--silent', '-si', help='Dont show hit/failure stats (Default: false)', action='store_true')
+
     parser.add_argument('--improvised', '-i', help='Whether the throw is improvised (Default: false)',
                         action='store_true')
+    parser.add_argument('--extended', '-e', help='Whether the dice roll is an extended probe (Default: disabled)',
+                        action='store_true')
+
     parser.add_argument('--finalStats', '-fS', help='Print timing stats at the end of execution (Default: false)',
                         action='store_true')
+
     parser.add_argument('--threshold', '-t', help='The threshold to succeed a d20 probe (Default: disabled)')
     parser.add_argument('--correction', '-c', help='The available correction points (Default: disabled)')
 
@@ -56,9 +173,16 @@ def main():
     try:
         args.rolls = int(args.rolls)
         args.sides = int(args.sides)
+        args.hits = int(args.hits)
+
         if args.threshold is not None:
-            args.threshold = int(args.threshold)
-            args.correction = int(args.correction)
+            try:
+                args.threshold = int(args.threshold)
+                args.correction = int(args.correction)
+
+            except TypeError:
+                print('ERROR: Make sure you use the threshold and correction parameters for a probe roll.')
+                exit(0)
 
     # catch conversion errors by invalid parameters and show the user the help hint
     except ValueError:
@@ -66,130 +190,38 @@ def main():
         exit(0)
 
     # start timer if stats were requested
+    t0 = 0
     if args.finalStats:
         t0 = time.time()
 
     # declare some variables
     if args.threshold is None:
-        hits = 0
-        misses = 0
+        rolls = {}
+        roll_amount = 0
 
-        dice_rolls = {}
+        if args.extended:
+            while not has_enough_hits(rolls, args.hits) and args.rolls > 0:
+                rolls = dice_roll_normal(6, args.rolls, rolls)
+                args.rolls -= 1
+                roll_amount += 1
 
-        current_roll = 0
+            hits = 0
+            fails = 0
+            if args.silent is False:
+                hits = rolls[args.sides - 1] + rolls[args.sides - 2]
+                fails = rolls[0]
 
-        # resize the dice roll list to the given dice size
-        for i in range(args.sides):
-            dice_rolls[i] = 0
+            print('HITS: {0}, FAILURES: {1} (In {2} rolls)'.format(hits, fails, roll_amount))
 
-        # roll the dice
-        for i in range(args.rolls):
-            current_roll = random.randint(1, args.sides)
-            dice_rolls[current_roll - 1] = dice_rolls[current_roll - 1] + 1
-
-            # collect hit / failure stats if parameter is given
-            if args.showHits:
-                if current_roll >= args.sides - 1:
-                    hits = hits + 1
-                if args.improvised:
-                    if current_roll <= 2:
-                        misses = misses + 1
-                else:
-                    if current_roll == 1:
-                        misses = misses + 1
-
-        # show the final stats if the parameter was given
-        if args.showHits:
-            print('HITS: ' + str(hits))
-            print('FAILURES: ' + str(misses))
-
-        # if there was only one roll requested, just print the result
-        if args.rolls == 1:
-            print(str(current_roll))
-
-        # otherwise, print all results if they got rolled at least once
         else:
-            for i in dice_rolls:
-                if dice_rolls[i] != 0:
-                    print(str(i + 1) + ': ' + str(dice_rolls[i]))
+            rolls = dice_roll_normal(args.sides, args.rolls)
+
+        for i in range(args.sides):
+            print('{0}: {1}'.format(i + 1, rolls[i]))
+
     else:
         # we are rolling a d20 probe
-        rolls = {}
-        successes = {}
-        twenties = {}
-
-        if args.rolls == 1:
-            args.rolls = 3
-
-        # roll the dice
-        for i in range(args.rolls):
-            rolls[i] = random.randint(1, 20)
-            if rolls[i] == 20:
-                twenties[i] = True
-            else:
-                twenties[i] = False
-
-            if rolls[i] <= args.threshold:
-                successes[i] = True
-            else:
-                successes[i] = False
-
-        print('ROLLS: {0}'.format(rolls))
-        # print('SUCCESSES: {0}'.format(successes))
-        print('CORRECTION: {0}'.format(args.correction))
-
-        # this is where we try to turn the highest possible amount of failures into successes
-        # loop while optimization is still possible
-        optimized = False
-        if any(a for a in rolls):
-            print('')
-            print('OPTIMIZING...')
-            while not optimized:
-                lowest_failure = 21
-                failure_id = -1
-                # find the lowest failure
-                print('')
-                print('Determining lowest failed roll...')
-                for i in range(args.rolls):
-                    if lowest_failure > rolls[i] > args.threshold:
-                        lowest_failure = rolls[i]
-                        failure_id = i
-                        print('New lowest failed roll: {0} (Roll ID {1})'.format(lowest_failure, failure_id))
-
-                if failure_id is not -1:
-                    # try to turn it into a success
-                    needed_correction = lowest_failure - args.threshold
-                    print('')
-                    print('Correction points needed: {0}, points available: {1}'.format(needed_correction, args.correction))
-                    if needed_correction <= args.correction:
-                        args.correction = args.correction - needed_correction
-                        rolls[failure_id] = rolls[failure_id] - needed_correction
-                        successes[failure_id] = True
-                        print('Roll {0} optimized'.format(failure_id))
-                    else:
-                        optimized = True
-                        print('All rolls optimized')
-                else:
-                    optimized = True
-                    print('All rolls optimized')
-
-        # determine final data
-        succs = 0
-        fails = 0
-        fixed_20s = 0
-
-        for i in rolls:
-            if successes[i]:
-                succs = succs + 1
-
-            if rolls[i] == 20:
-                fails = fails + 1
-            elif twenties[i]:
-                fixed_20s = fixed_20s + 1
-
-        # print the final probe result
-        print('')
-        print('FINAL RESULT: {0} SUCCESS(ES), {1} x 20, {2} x CORRECTED 20'.format(succs, fails, fixed_20s))
+        dice_roll_probe(args.rolls, args.threshold, args.correction)
 
     # stop time and calculate speed
     if args.finalStats:
